ecc9bcae5dd1a4eefe2ccbd8c275267269fcd449
Kevin Kelley <github@wyldwolf.org>
2014-04-14 17:05:48 -0500

    Initial commit of 'md2html.py' and 'pyblog.py'

---
+++ md2html.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python
+###############################################################################
+# 'md2html.py'
+#
+# A simple library to convert basic markdown text to HTML using the 'markup.py'
+# library.
+
+###############################################################################
+# Configuration
+
+import markup
+
+###############################################################################
+# Code
+
+class Text(object):
+	def __init__(self):
+		pass
+
+	def __del__(self):
+		pass
+
+	def configure(self, configuration=None):
+		if configuration is None:
+			# Error, no configuration provided
+			pass
+
+		if type(configuration) is not type({}):
+			# Error, configuration is not the correct data type
+			pass
+
+		# List of required configuration items
+		req = ['', '']
+
+		for i in req:
+			if i not in configuration.keys():
+				# Error, a required configuration item was not included
+				pass
+
+		# Configure the instance
+		self.configuration_item = configuration[configuration_item]
+
+		return None
+
+	def convert(self):
+		pass
+
+	def entity_encode(self):
+		pass
+
+###############################################################################
+# Main
+
+if __name__ == "__main__":
+	# Add code to support single file conversion from command-line.
+	pass
+
+###############################################################################
+# End
---
+++ pyblog.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+###############################################################################
+# 'pyblog.py'
+#
+# About: A very basic static site generator written in Python. Site data is
+#  stored in plain-text files in a single folder (support for archives (tar,
+#  zip, xz, bz2, etc...) will be added at a later date). Support a subset of
+#  markdown via the 'md2html' python library.
+#
+# Version: 0.01
+# Author: Kevin Kelley [kevin@thesta.in] (http://thesta.in)
+# License: WTFPL
+
+###############################################################################
+# Configuration
+
+import glob
+
+import md2html
+
+###############################################################################
+# Code
+
+def main(configuration=None):
+
+
+###############################################################################
+# End
